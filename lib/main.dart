f61f0ed116ec2bd17d24a7332e027dcd559caf89
somnathsps <somnath_mistry@softprodigy.com>
2022-10-27 17:38:49 +0530

    3nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Testing app somanth',
+      title: 'Testing app somanth this isit',
       theme: ThemeData(
         // This is the theme of your application.
         //
